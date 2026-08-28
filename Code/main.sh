b7d9174162fb85f6622a704a2dfd8cf67fd2fc46
Prajwal K <prajwal220pp@gmail.com>
2026-08-28 17:32:35 +0530

    Implement main menu for LSAT
    Added a menu for Linux System Administration Toolkit with options for user management, permission management, disk usage analysis, log analysis, backup management, and system information.

--- Code/main.sh
+++ Code/main.sh
@@ -1,1 +1,55 @@
+#!/bin/bash
 
+clear
+
+echo "==============================================="
+echo " Linux System Administration Toolkit (LSAT)"
+echo "==============================================="
+
+echo
+echo "1. User Management"
+echo "2. Permission Management"
+echo "3. Disk Usage Analyzer"
+echo "4. Log Analyzer"
+echo "5. Backup Manager"
+echo "6. System Information"
+echo "7. Exit"
+
+echo
+read -p "Enter your choice: " choice
+
+case $choice in
+
+1)
+    ./user_management.sh
+    ;;
+
+2)
+    ./permission_management.sh
+    ;;
+
+3)
+    ./disk_usage.sh
+    ;;
+
+4)
+    ./log_analyzer.sh
+    ;;
+
+5)
+    ./backup.sh
+    ;;
+
+6)
+    ./system_info.sh
+    ;;
+
+7)
+    exit
+    ;;
+
+*)
+    echo "Invalid Choice"
+    ;;
+
+esac
