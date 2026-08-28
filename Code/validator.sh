8818c1ee1d2b199400fa69dac7a7400d2f3d8b3d
Prajwal K <prajwal220pp@gmail.com>
2026-08-28 17:35:43 +0530

    Implement existence check functions in validator.sh
    Added functions to check for file, directory, and user existence.

--- Code/validator.sh
+++ Code/validator.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+file_exists(){
+
+if [ -f "$1" ]
+then
+    return 0
+else
+    return 1
+fi
+
+}
+
+directory_exists(){
+
+if [ -d "$1" ]
+then
+    return 0
+else
+    return 1
+fi
+
+}
+
+user_exists(){
+
+id "$1" &>/dev/null
+
+}
