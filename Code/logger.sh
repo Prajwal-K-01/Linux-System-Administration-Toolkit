ca369a9d50f828bf827a9ec3342b35c837c2fd0f
Prajwal K <prajwal220pp@gmail.com>
2026-08-28 17:32:03 +0530

    Implement log_action function in logger.sh
    Added a logging function to record actions with timestamps and user info.

--- Code/logger.sh
+++ Code/logger.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+
+source config.sh
+
+
+log_action(){
+	
+	local action="$1"
+	local status="${2:-SUCCESS}"
+
+	local timestamp
+	local username
+
+	timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+	username=$(whoami)
+
+	mkdir -p "$LOG_DIR"
+
+	cat >> "$LOG_FILE" <<EOF
+
+-----------------------------------------------
+Date     :  $(date '+%Y-%m-%d')
+Time     :  $(date '+%H:%M:%S')
+User     :  $username
+Action   :  $action
+Status   :  $status
+----------------------------------------------
+EOF
+
+
+}
